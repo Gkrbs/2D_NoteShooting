fbeada32b589758501c21f1428be597cada4616a
Dilrong <wet932@naver.com>
2018-01-06 17:33:13 +0900

    REV alph

---
+++ Assets/Res/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+    [SerializeField] Background target;
+    private float speed;
+	// Use this for initialization
+	void Start () {
+        speed = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > -9.9f)
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        else
+            transform.position = target.transform.position + new Vector3(0.0f, 9.9f, 0.0f);
+	}
+}
---
+++ Assets/Res/Scripts/Character.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 5.0f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (transform.position.y < 4.5f)
+                transform.position += Vector3.up * speed * Time.deltaTime;
+            else
+                transform.position = new Vector3(transform.position.x, 4.5f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (transform.position.y > -4.5f)
+                transform.position += Vector3.down * speed * Time.deltaTime;
+            else
+                transform.position = new Vector3(transform.position.x, -4.5f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -3.0f)
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            else
+                transform.position = new Vector3(-3.0f, transform.position.y, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 3.0f)
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            else
+                transform.position = new Vector3(3.0f, transform.position.y, 0.0f);
+        }
+    }
+}
